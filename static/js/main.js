c9104f00c3a7f212d28c7bd1e6cf9a03923290a9
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2012-08-12 16:25:44 +0530

    added ajax request loading indicator

--- static/js/main.js
+++ static/js/main.js
@@ -2,34 +2,23 @@ $(document).ready(function() {
 
     refreshtweet();
     $('#refreshtweet').click(refreshtweet);
-    
+
     $('#changehashtag').click(function() {
         var newhashtag = prompt("New hashtag?", "#openhackindia");
         $('#hashtag').text(newhashtag);
         refreshtweet();
     });
     
-    $('#changelocation').click(function() {
-        var newlocation = prompt("New location?", "Sheraton, Bangalore");
-        $.get('location/' + newlocation, function(data) {
-           newhashtags = JSON.parse(data);
-           for (var hashtag in newhashtags) {
-           }
-           // Display hashtags in a list
-           // When clicked on an item in the list,
-           // set that as the new hashtag and call
-           // refreshtweet
-        });
-    });
-
     $('#tweetnow').click(function() {
+        $('#tweetnow').text('Tweeting...');
         var tweet = $('#tweettext').text();
         $.get('tweet/' + escape(tweet), function(data) {
-            console.log("Tweeted: " + escape(tweet));
+            $('#tweetnow').text('Tweet');
         });
     });
 
     function refreshtweet() {
+        $('#tweettext').text('Loading...');
         var hashtag = $('#hashtag').text();
         hashtag = hashtag.substr(1, hashtag.length);
         $.get('generate/' + hashtag, function(data) {
